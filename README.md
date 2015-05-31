5616e685a7110e3887ce07798f49ed799729db2e
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2015-05-31 03:58:28 -0400

    updating readme

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # ShaderLog
 
+## Install
+
+First install a console git client, [Node.js](https://nodejs.org/) and [tPrint](https://github.com/patriciogonzalezvivo/tprint) then do:
+
+```bash
+cd ~
+git clone https://github.com/patriciogonzalezvivo/shaderLog.git
+cd ~/shaderLog
+npm install -l
+```
+
+## Run
+
+```bash
+cd ~/shaderLog
+npm start
+```
+
 ## Author
 
 [Patricio Gonzalez Vivo](http://https://twitter.com/patriciogv): [github](https://github.com/patriciogonzalezvivo) | [twitter](http://https://twitter.com/patriciogv) | [website](http://patricio.io)
--- install.sh
+++ install.sh
@@ -5,12 +5,10 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install avahi-daemon git-core
 
-# Install GLSL-VIEWER
+# Install NODE.JS
 cd ~
-git clone https://github.com/patriciogonzalezvivo/glslViewer.git
-cd glslViewer
-make
-sudo make install
+wget http://node-arm.herokuapp.com/node_latest_armhf.deb
+sudo dpkg -i node_latest_armhf.deb
 
 # Install TPRINT
 cd ~
@@ -18,11 +16,6 @@ git clone https://github.com/patriciogonzalezvivo/tprint.git
 cd tprint
 make
 sudo make install
-
-# Install NODE.JS
-cd ~
-wget http://node-arm.herokuapp.com/node_latest_armhf.deb
-sudo dpkg -i node_latest_armhf.deb
 
 cd ~/shaderLog
 npm install -l
