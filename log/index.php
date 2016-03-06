5dc318cc0c61b4fd68bb616dee8222ac9a41b874
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2016-03-06 11:41:39 -0500

    stand alone

--- log/index.php
+++ log/index.php
@@ -1,6 +1,28 @@
 <?php 
-    $path = "..";
-    include($path."/header.php");
+// main menu
+echo '
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>The Book of Shaders Gallery</title>
+        <meta name="keywords" content="shader,openGL,WebGL,GLSL,procedural,generative" />
+        <meta name="description" content="Gallery of Fragment Shaders of The Book of Shaders" />
+
+        <meta name="twitter:site" content="@bookofshaders">
+        <meta name="twitter:title" content="The Book Of Shaders">
+        <meta name="twitter:description" content="Gentle step-by-step guide through the abstract and complex universe of Fragment Shaders.">
+        <meta name="twitter:creator" content="@patriciogv">
+        <meta name="twitter:domain" content="thebookofshaders.edu">
+        <link href="/favicon.gif" rel="shortcut icon"/>
+
+        <!-- GlslCanvas -->
+        <script type="text/javascript" src="https://rawgit.com/patriciogonzalezvivo/glslCanvas/master/build/GlslCanvas.min.js"></script>
+
+        <link type="text/css" rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+    ';
 
     $images = array_reverse(glob("*.png"));
 
@@ -9,23 +31,36 @@
     <hr>
     <div class="gallery">';
 
-foreach ($images as &$img) {
-    $log = basename($img, ".png");
-    echo '
-    <div class="gallery_item">
-        <a href="http://editor.thebookofshaders.com/?log='.$log.'" target="_blank">
-            <img class="gallery_thumb" src="'.$img.'" alt="">
-            <p> '.$log.'</p>
-        </a>
-    </div>';
-}
+    foreach ($images as &$img) {
+        $log = basename($img, ".png");
+        echo '
+        <div class="gallery_item">
+            <a href="http://editor.thebookofshaders.com/?log='.$log.'" target="_blank">
+                <img class="gallery_thumb" src="'.$img.'" alt="">
+                <p> '.$log.'</p>
+            </a>
+        </div>';
+    }
 
     echo '
-    </div>
-    <hr>
-    <ul class="navigationBar" >
-        <li class="navigationBar" onclick="homePage()"> Home </li>
-    </ul>';
+        </div>
+        <hr>
+        <ul class="navigationBar" >
+            <li class="navigationBar"><a href="http://thebookofshaders.com/"> Home </a></li>
+        </ul>
+        <footer>
+            <p> Copyright 2015 <a href="http://www.patriciogonzalezvivo.com" target="_blank">Patricio Gonzalez Vivo</a> </p>
+        </footer>
 
-    include($path."/footer.php"); 
+        <script>
+          (function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,"script","//www.google-analytics.com/analytics.js","ga");
+
+          ga("create", "UA-18824436-2", "auto");
+          ga("send", "pageview");
+        </script>
+    </body>
+</html>';
 ?>
